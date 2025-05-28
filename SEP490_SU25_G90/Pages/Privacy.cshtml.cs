9a66f3bb641ca95a8159179dd46a7a6687a52462
lampthe162003 <lampthe162003@fpt.edu.vn>
2025-05-28 09:36:24 +0700

    Initial Commit

---
+++ SEP490_SU25_G90/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SEP490_SU25_G90.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
