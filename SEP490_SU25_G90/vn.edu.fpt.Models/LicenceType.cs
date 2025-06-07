246f413ca3d46ff25323dd01958e6d5276613070
lampthe162003 <lampthe162003@fpt.edu.vn>
2025-06-07 15:43:44 +0700

    Add scaffolded models

--- SEP490_SU25_G90/vn.edu.fpt.Models/LicenceType.cs
+++ SEP490_SU25_G90/vn.edu.fpt.Models/LicenceType.cs
@@ -9,9 +9,13 @@ public partial class LicenceType
 
     public string LicenceCode { get; set; } = null!;
 
+    public virtual ICollection<Course> Courses { get; set; } = new List<Course>();
+
     public virtual ICollection<InstructorSpecialization> InstructorSpecializations { get; set; } = new List<InstructorSpecialization>();
 
     public virtual ICollection<LearningApplication> LearningApplications { get; set; } = new List<LearningApplication>();
+
+    public virtual ICollection<LearningMaterial> LearningMaterials { get; set; } = new List<LearningMaterial>();
 
     public virtual ICollection<MockTestQuestion> MockTestQuestions { get; set; } = new List<MockTestQuestion>();
 }
