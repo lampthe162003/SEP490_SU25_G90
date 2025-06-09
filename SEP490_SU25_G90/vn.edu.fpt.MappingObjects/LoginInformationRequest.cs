9a090bfb70a666e15400ca3bc38193b8e8e78da5
lampthe162003 <lampthe162003@fpt.edu.vn>
2025-06-09 21:55:49 +0700

    New Service layer for User

--- SEP490_SU25_G90/vn.edu.fpt.MappingObjects/LoginInformationRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SEP490_SU25_G90.vn.edu.fpt.MappingObjects
-{
-    public class LoginInformationRequest
-    {
-        public string? Username { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
-    }
-}
---
+++ SEP490_SU25_G90/vn.edu.fpt.Services/User/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using SEP490_SU25_G90.vn.edu.fpt.MappingObjects;
+
+namespace SEP490_SU25_G90.vn.edu.fpt.Services.User
+{
+    public interface IUserService
+    {
+        public List<UserListInformationResponse> GetAllUsers();
+        
+    }
+}
