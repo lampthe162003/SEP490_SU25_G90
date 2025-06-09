06bbc9aad31ba509ae39937bc76e16ac5329814b
TrungNP <trungnphe151193@fpt.edu.vn>
2025-06-09 11:41:03 +0700

    ListCarCourse

---
+++ SEP490_SU25_G90/Pages/Admins/CarCourse/List.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SEP490_SU25_G90.vn.edu.fpt.Models;
+
+namespace SEP490_SU25_G90.Pages.Admins.CarCourse
+{
+    public class ListModel : PageModel
+    {
+        private readonly SEP490_SU25_G90.vn.edu.fpt.Models.Sep490Su25G90DbContext _context;
+
+        public ListModel(SEP490_SU25_G90.vn.edu.fpt.Models.Sep490Su25G90DbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Courses
+                .Include(c => c.LicenceType)
+                .Include(c => c.Videos) 
+                .ToListAsync();
+        }
+
+    }
+}
