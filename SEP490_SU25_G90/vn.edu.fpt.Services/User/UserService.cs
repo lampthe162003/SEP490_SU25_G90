f75a66ccce4bba780dc1b1e188212201068a754f
lampthe162003 <lampthe162003@fpt.edu.vn>
2025-06-09 21:55:49 +0700

    New Service layer for User

---
+++ SEP490_SU25_G90/vn.edu.fpt.Services/User/UserService.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.Metadata;
+using SEP490_SU25_G90.vn.edu.fpt.MappingObjects;
+using SEP490_SU25_G90.vn.edu.fpt.Models;
+using SEP490_SU25_G90.vn.edu.fpt.Repositories.UserRepository;
+
+namespace SEP490_SU25_G90.vn.edu.fpt.Services.User
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserService(Sep490Su25G90DbContext context, IMapper mapper)
+        {
+            _userRepository = new UserRepository(context);
+            _mapper = mapper;
+        }
+        public List<UserListInformationResponse> GetAllUsers() 
+            => _mapper.Map<List<UserListInformationResponse>>(_userRepository.GetAllUsers());
+    }
+}
