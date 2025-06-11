a52f063b250023ee64a4f2895c9e17f278fcc24f
lampthe162003 <lampthe162003@fpt.edu.vn>
2025-06-11 07:35:07 +0700

    change return types of List into IQueryable to chain queries for searching

--- SEP490_SU25_G90/vn.edu.fpt.Repositories/UserRepository/IUserRepository.cs
+++ SEP490_SU25_G90/vn.edu.fpt.Repositories/UserRepository/IUserRepository.cs
@@ -4,13 +4,10 @@ namespace SEP490_SU25_G90.vn.edu.fpt.Repositories.UserRepository
 {
     public interface IUserRepository
     {
-        public List<User> GetAllUsers();
-        public User GetById(int id);
-        public List<User> GetByName(string name);
-        public List<User> GetByEmail(string email);
-        public User GetByUsername(string username);
+        public IQueryable<User> GetAllUsers();
+        public IQueryable<User> GetUsersByEmail(IQueryable<User> query, string email);
+        public IQueryable<User> GetUsersByName(IQueryable<User> query, string name);
         public void Create(User user);
         public void Update(User user);
-        public List<User> GetByCccd(string cccd);
     }
 }
