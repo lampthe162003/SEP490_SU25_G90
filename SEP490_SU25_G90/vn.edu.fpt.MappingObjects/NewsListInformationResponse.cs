f2712cad998d838d7a94ddf37138ff4c23e57970
phongthhe163965 <phongthhe163965@fpt.edu.vn>
2025-06-24 14:52:20 +0700

    Add dto

---
+++ SEP490_SU25_G90/vn.edu.fpt.MappingObjects/NewsListInformationResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace SEP490_SU25_G90.vn.edu.fpt.MappingObjects
+{
+    public class NewsListInformationResponse
+    {
+        public int NewsId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string NewsContent { get; set; } = string.Empty ;
+        public string AuthorName { get; set; } = string.Empty;
+        public DateTime? PostTime { get; set; }
+        public string ShortContent { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+    }
+}
