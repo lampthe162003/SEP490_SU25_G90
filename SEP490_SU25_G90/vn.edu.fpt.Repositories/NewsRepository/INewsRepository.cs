5e84d22575f087679b5109af2ae1940c6bd99800
phongthhe163965 <phongthhe163965@fpt.edu.vn>
2025-06-24 14:51:33 +0700

    Add new repository

---
+++ SEP490_SU25_G90/vn.edu.fpt.Repositories/NewsRepository/INewsRepository.cs
@@ -0,0 +1,10 @@
+﻿using SEP490_SU25_G90.vn.edu.fpt.Models;
+
+namespace SEP490_SU25_G90.vn.edu.fpt.Repositories.NewsRepository
+{
+    public interface INewsRepository
+    {
+        Task<(List<News>, int)> GetPagedNewsAsync(int page, int pageSize);
+        Task<News?> GetNewsByIdAsync(int id);
+    }
+}
---
+++ SEP490_SU25_G90/vn.edu.fpt.Repositories/NewsRepository/NewsRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SEP490_SU25_G90.vn.edu.fpt.Models;
+
+namespace SEP490_SU25_G90.vn.edu.fpt.Repositories.NewsRepository
+{
+    public class NewsRepository : INewsRepository
+    {
+        private readonly Sep490Su25G90DbContext _context;
+
+        public NewsRepository(Sep490Su25G90DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<(List<News>, int)> GetPagedNewsAsync(int page, int pageSize)
+        {
+            var totalItems = await _context.News.CountAsync();
+
+            var items = await _context.News
+                .OrderByDescending(n => n.PostTime) 
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return (items, totalItems);
+        }
+        public async Task<News?> GetNewsByIdAsync(int id)
+        {
+            return await _context.News
+                .Include(n => n.Author)
+                .FirstOrDefaultAsync(n => n.NewsId == id);
+        }
+    }
+}
