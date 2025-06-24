6d0c8237300a91b44f10bda8ac3d7313c17269b4
phongthhe163965 <phongthhe163965@fpt.edu.vn>
2025-06-24 14:51:51 +0700

    add newsservice

---
+++ SEP490_SU25_G90/vn.edu.fpt.Services/NewsService/INewsService.cs
@@ -0,0 +1,10 @@
+﻿using SEP490_SU25_G90.vn.edu.fpt.MappingObjects;
+
+namespace SEP490_SU25_G90.vn.edu.fpt.Services.NewsService
+{
+    public interface INewsService
+    {
+        Task<(List<NewsListInformationResponse>, int)> GetPagedNewsAsync(int page, int pageSize);
+        Task<NewsListInformationResponse?> GetNewsByIdAsync(int id);
+    }
+}
