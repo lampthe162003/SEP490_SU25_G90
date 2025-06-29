a3f017dc4c116d34127b4f180842d778fb83f9bb
kiet <kieththe141560@fpt.edu.vn>
2025-06-30 04:14:43 +0700

    Update InstructorListInformationResponse.cs

--- SEP490_SU25_G90/vn.edu.fpt.MappingObjects/InstructorListInformationResponse.cs
+++ SEP490_SU25_G90/vn.edu.fpt.MappingObjects/InstructorListInformationResponse.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SEP490_SU25_G90.vn.edu.fpt.MappingObjects
 {
     public class InstructorListInformationResponse
@@ -32,5 +34,40 @@ namespace SEP490_SU25_G90.vn.edu.fpt.MappingObjects
     {
         public byte LicenceTypeId { get; set; }
         public string LicenceCode { get; set; } = null!;
+    }
+
+    public class UpdateInstructorRequest
+    {
+        public int UserId { get; set; }
+        
+        [Required(ErrorMessage = "Email là bắt buộc")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        public string? Email { get; set; }
+        
+        [Required(ErrorMessage = "Họ là bắt buộc")]
+        [StringLength(50, ErrorMessage = "Họ không được quá 50 ký tự")]
+        public string? FirstName { get; set; }
+        
+        [StringLength(50, ErrorMessage = "Tên đệm không được quá 50 ký tự")]
+        public string? MiddleName { get; set; }
+        
+        [Required(ErrorMessage = "Tên là bắt buộc")]
+        [StringLength(50, ErrorMessage = "Tên không được quá 50 ký tự")]
+        public string? LastName { get; set; }
+        
+        public DateOnly? Dob { get; set; }
+        public bool? Gender { get; set; }
+        
+        [Phone(ErrorMessage = "Số điện thoại không hợp lệ")]
+        public string? Phone { get; set; }
+        
+        [RegularExpression(@"^(\d{12})?$", ErrorMessage = "Số CCCD phải có đúng 12 chữ số và chỉ chứa số")]
+        public string? CccdNumber { get; set; }
+        
+        public string? CccdImageFront { get; set; }
+        
+        public string? CccdImageBack { get; set; }
+        
+        public List<byte> SelectedSpecializations { get; set; } = new List<byte>();
     }
 }
