2fce9b09b3b1701feac72f41cfc1dfa973305f92
phongthhe163965 <phongthhe163965@fpt.edu.vn>
2025-07-14 13:11:14 +0700

    add authorize for learnern guest

--- SEP490_SU25_G90/Pages/Learner/News/ListNews.cshtml.cs
+++ SEP490_SU25_G90/Pages/Learner/News/ListNews.cshtml.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 
 namespace SEP490_SU25_G90.Pages.Learner.News
 {
+    [Authorize(Policy = "GuestOrLearnerPolicy")]
     public class ListNewsModel : PageModel
     {
         private readonly INewsService _iNewsService;
