435fe58e9ded33574b2a9d14080835ac0b5f3f85
lampthe162003 <lampthe162003@fpt.edu.vn>
2025-07-15 20:03:59 +0700

    Add new page to display internal server error

---
+++ SEP490_SU25_G90/Pages/Error500.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SEP490_SU25_G90.Pages
+{
+    public class Error500Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
