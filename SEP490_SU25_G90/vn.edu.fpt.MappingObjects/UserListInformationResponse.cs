697118c8fb56880859d32c526a355b9ace6a4b3f
lampthe162003 <lampthe162003@fpt.edu.vn>
2025-07-16 23:19:51 +0700

    add display names to the properties

--- SEP490_SU25_G90/vn.edu.fpt.MappingObjects/UserListInformationResponse.cs
+++ SEP490_SU25_G90/vn.edu.fpt.MappingObjects/UserListInformationResponse.cs
@@ -1,4 +1,5 @@
 ﻿using SEP490_SU25_G90.vn.edu.fpt.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace SEP490_SU25_G90.vn.edu.fpt.MappingObjects
 {
@@ -8,12 +9,16 @@ namespace SEP490_SU25_G90.vn.edu.fpt.MappingObjects
 
         public string? Email { get; set; }
 
+        [Display(Name = "Họ Tên")]
         public string? FullName { get; set; }
 
+        [Display(Name = "Ngày Sinh")]
         public DateOnly? Dob { get; set; }
 
+        [Display(Name = "Giới Tính")]
         public bool? Gender { get; set; }
 
+        [Display(Name = "Số Điện Thoại")]
         public string? Phone { get; set; }
 
         public virtual ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
