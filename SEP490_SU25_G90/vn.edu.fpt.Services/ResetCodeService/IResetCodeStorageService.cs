449451b8d4dabecf4e3b82a7251a41464f36edec
lampthe162003 <lampthe162003@fpt.edu.vn>
2025-07-17 17:17:47 +0700

    new service to store reset codes

---
+++ SEP490_SU25_G90/vn.edu.fpt.Services/ResetCodeService/IResetCodeStorageService.cs
@@ -0,0 +1,9 @@
+﻿namespace SEP490_SU25_G90.vn.edu.fpt.Services.ResetCodeService
+{
+    public interface IResetCodeStorageService
+    {
+        void SaveCode(string email, string code, TimeSpan expiresIn);
+        (string Code, DateTime ExpiresAt)? GetCode(string email);
+        void RemoveCode(string email);
+    }
+}
---
+++ SEP490_SU25_G90/vn.edu.fpt.Services/ResetCodeService/ResetCodeStorageService.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Concurrent;
+
+namespace SEP490_SU25_G90.vn.edu.fpt.Services.ResetCodeService
+{
+    public class ResetCodeStorageService : IResetCodeStorageService
+    {
+        private readonly ConcurrentDictionary<string, (string code, DateTime expiresAt)> _storage = new();
+
+        public (string Code, DateTime ExpiresAt)? GetCode(string email)
+        {
+            if (_storage.TryGetValue(email, out var value))
+            {
+                return value;
+            }
+            return null;
+        }
+
+        public void RemoveCode(string email)
+        {
+            _storage.TryRemove(email, out _);
+        }
+
+        public void SaveCode(string email, string code, TimeSpan expiresIn)
+        {
+            _storage[email] = (code, DateTime.UtcNow.Add(expiresIn));
+        }
+    }
+}
