b30380b597c2173dacab65fa4a77b353832ab3b3
lampthe162003 <lampthe162003@fpt.edu.vn>
2025-07-21 01:19:47 +0700

    Repository and service layer for ScheduleSlots

---
+++ SEP490_SU25_G90/vn.edu.fpt.Repositories/ScheduleSlotRepository/IScheduleSlotRepository.cs
@@ -0,0 +1,12 @@
+﻿using SEP490_SU25_G90.vn.edu.fpt.Models;
+
+namespace SEP490_SU25_G90.vn.edu.fpt.Repositories.ScheduleSlotRepository
+{
+    public interface IScheduleSlotRepository
+    {
+        public Task<IList<ScheduleSlot>> GetAllSlots();
+        public Task<ScheduleSlot> GetSlotById(int slotId);
+        public Task AddSlot(ScheduleSlot scheduleSlot);
+        public Task UpdateSlot(ScheduleSlot scheduleSlot);
+    }
+}
