59bcd1af93d72789479936a26038535716688e61
lampthe162003 <lampthe162003@fpt.edu.vn>
2025-07-21 01:19:29 +0700

    Repository and service layer for Car

---
+++ SEP490_SU25_G90/vn.edu.fpt.Services/CarService/CarService.cs
@@ -0,0 +1,35 @@
+﻿using SEP490_SU25_G90.vn.edu.fpt.Models;
+using SEP490_SU25_G90.vn.edu.fpt.Repositories.CarRepository;
+
+namespace SEP490_SU25_G90.vn.edu.fpt.Services.CarService
+{
+    public class CarService : ICarService
+    {
+        private readonly ICarRepository _carRepository;
+
+        public CarService(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        public async Task AddCar(Car car)
+        {
+            await _carRepository.AddCarAsync(car);
+        }
+
+        public async Task<IList<Car>> GetAllCars()
+        {
+            return await _carRepository.GetAllCarsAsync();
+        }
+
+        public async Task<Car> GetCarById(int carId)
+        {
+            return await _carRepository.GetCarByIdAsync(carId);
+        }
+
+        public async Task UpdateCar(Car car)
+        {
+            await _carRepository.UpdateCarAsync(car);
+        }
+    }
+}
---
+++ SEP490_SU25_G90/vn.edu.fpt.Services/CarService/ICarService.cs
@@ -0,0 +1,12 @@
+﻿using SEP490_SU25_G90.vn.edu.fpt.Models;
+
+namespace SEP490_SU25_G90.vn.edu.fpt.Services.CarService
+{
+    public interface ICarService
+    {
+        public Task<IList<Car>> GetAllCars();
+        public Task<Car> GetCarById(int carId);
+        public Task AddCar(Car car);
+        public Task UpdateCar(Car car);
+    }
+}
