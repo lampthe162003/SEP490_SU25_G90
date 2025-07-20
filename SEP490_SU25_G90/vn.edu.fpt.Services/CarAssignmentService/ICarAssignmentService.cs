622278a08cd17f4f6087dab27d855b10a7f2d62e
kiet <kieththe141560@fpt.edu.vn>
2025-07-20 17:29:48 +0700

    Create ICarAssignmentService.cs

---
+++ SEP490_SU25_G90/vn.edu.fpt.Services/CarAssignmentService/ICarAssignmentService.cs
@@ -0,0 +1,29 @@
+using SEP490_SU25_G90.vn.edu.fpt.MappingObjects.CarAssignment;
+using SEP490_SU25_G90.vn.edu.fpt.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SEP490_SU25_G90.vn.edu.fpt.Services.CarAssignmentService
+{
+    public interface ICarAssignmentService
+    {
+        Task<List<CarAssignmentResponse>> GetAllCarAssignmentsAsync();
+        Task<List<CarAssignmentResponse>> GetAllCarsWithAssignmentsAsync();
+        Task<List<CarAssignmentResponse>> SearchCarAssignmentsAsync(CarAssignmentSearchRequest request, int? currentInstructorId = null);
+        Task<CarAssignmentResponse?> GetCarAssignmentByIdAsync(int assignmentId);
+        Task<bool> RentCarAsync(CarRentalRequest request);
+        Task<bool> ReturnCarAsync(int assignmentId);
+        Task<bool> UpdateCarAssignmentAsync(int assignmentId, CarRentalRequest request);
+        Task<bool> DeleteCarAssignmentAsync(int assignmentId);
+        
+        // Helper methods cho dropdown lists
+        Task<List<SelectListItem>> GetCarMakesSelectListAsync();
+        Task<List<SelectListItem>> GetScheduleSlotsSelectListAsync();
+        Task<List<SelectListItem>> GetAvailableCarsSelectListAsync(DateOnly date, int slotId);
+        Task<List<SelectListItem>> GetInstructorsSelectListAsync();
+        
+        // Validation methods
+        Task<bool> IsCarAvailableAsync(int carId, DateOnly date, int slotId);
+        Task<bool> CanInstructorRentCarAsync(int instructorId, DateOnly date, int slotId);
+        Task<List<CarAssignmentResponse>> GetCarAssignmentsByInstructorAsync(int instructorId, DateOnly date, int slotId);
+    }
+} 
