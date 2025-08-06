a357be87c31753e7a60bb82efb8d4449f9410e8f
tuanddhe-151088 <tuanddhe151088@fpt.edu.vn>
2025-08-06 07:55:49 +0700

    Update IClassService.cs

--- SEP490_SU25_G90/vn.edu.fpt.Services/ClassService/IClassService.cs
+++ SEP490_SU25_G90/vn.edu.fpt.Services/ClassService/IClassService.cs
@@ -21,5 +21,19 @@ namespace SEP490_SU25_G90.vn.edu.fpt.Services.ClassService
         /// </summary>
         /// <returns>Danh sách loại bằng lái</returns>
         Task<IEnumerable<LicenceType>> GetAllLicenceTypesAsync();
+        
+        /// <summary>
+        /// Lấy chi tiết lớp học theo ID
+        /// </summary>
+        /// <param name="classId">ID của lớp học</param>
+        /// <returns>Chi tiết lớp học</returns>
+        Task<ClassDetailResponse?> GetClassDetailAsync(int classId);
+        
+        /// <summary>
+        /// Kiểm tra lớp học có tồn tại không
+        /// </summary>
+        /// <param name="classId">ID của lớp học</param>
+        /// <returns>True nếu tồn tại</returns>
+        Task<bool> ClassExistsAsync(int classId);
     }
 }
