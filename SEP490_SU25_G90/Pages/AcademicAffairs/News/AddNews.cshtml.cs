e65058610540c229f7ec7ec06f3aae6587e04ec5
phongthhe163965 <phongthhe163965@fpt.edu.vn>
2025-08-10 23:19:46 +0700

    update news

---
+++ SEP490_SU25_G90/Pages/AcademicAffairs/News/AddNews.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SEP490_SU25_G90.vn.edu.fpt.MappingObjects.News;
+using SEP490_SU25_G90.vn.edu.fpt.Models;
+using SEP490_SU25_G90.vn.edu.fpt.Services.NewsService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEP490_SU25_G90.Pages.AcademicAffairs.News
+{
+    [Authorize(Roles = "academic affairs")]
+    public class AddNewsModel : PageModel
+    {
+        private readonly INewsService _newsService;
+
+        public AddNewsModel(INewsService newsService)
+        {
+            _newsService = newsService;
+        }
+
+        [BindProperty]
+        public NewsFormRequest Input { get; set; } = new();
+
+        public void OnGet() { }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            var userIdClaim = User.FindFirst("user_id")?.Value;
+            if (userIdClaim == null) return Unauthorized();
+
+            int authorId = int.Parse(userIdClaim);
+            await _newsService.AddNewsAsync(Input, authorId);
+
+            return RedirectToPage("/AcademicAffairs/News/ListNews");
+        }
+    }
+}
