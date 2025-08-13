1c8212dbc094865e6f71e1fd4746e4c4647bfdd5
lampthe162003 <lampthe162003@fpt.edu.vn>
2025-08-13 09:48:33 +0700

    new class to seed testing data for UserService

---
+++ SEP490_SU25_G90/vn.edu.fpt.Commons/TestDataSeeding/UserServiceSeeding.cs
@@ -0,0 +1,15 @@
+﻿using SEP490_SU25_G90.vn.edu.fpt.Models;
+
+namespace SEP490_SU25_G90.vn.edu.fpt.Commons.TestDataSeeding
+{
+    public class UserServiceSeeding
+    {
+        public List<User> SeedGetAllUsersItems()
+        {
+            return new List<User>
+            {
+                new User { Email = "test.learner@example.com", FirstName = "Trần", MiddleName = "Văn", LastName = "B"}
+            };
+        }
+    }
+}
