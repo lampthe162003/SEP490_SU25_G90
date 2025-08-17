e7883dda736c562f39dd206b03f4a70082e76174
kiet <kieththe141560@fpt.edu.vn>
2025-08-17 18:22:58 +0700

    Update IUserService.cs

--- SEP490_SU25_G90/vn.edu.fpt.Services/UserService/IUserService.cs
+++ SEP490_SU25_G90/vn.edu.fpt.Services/UserService/IUserService.cs
@@ -9,6 +9,7 @@ namespace SEP490_SU25_G90.vn.edu.fpt.Services.UserService
         public Task CreateAccount(AccountCreationRequest request, byte roleId);
         public Task<List<LearnerUserResponse>> GetAllLearnersAsync();
         public Task<LearnerDetailResponse?> GetLearnerById(int id);
+        public Task<string> CreateLearnerAsync(CreateLearnerRequest request);
         public Task UpdateLearnerInfoAsync(int userId, UpdateLearnerRequest request);
         public Task UpdatePasswordAsync(int userId, string newPassword);
         public Task<UserListInformationResponse> GetUserDetailsAsync(int userId);
