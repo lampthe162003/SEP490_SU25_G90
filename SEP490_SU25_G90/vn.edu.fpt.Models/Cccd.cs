dcad672699ab1d660aeaac060c505ca4a0cc9dc1
lampthe162003 <lampthe162003@fpt.edu.vn>
2025-08-17 16:33:23 +0700

    add data annotations for validating CCCD number

--- SEP490_SU25_G90/vn.edu.fpt.Models/Cccd.cs
+++ SEP490_SU25_G90/vn.edu.fpt.Models/Cccd.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SEP490_SU25_G90.vn.edu.fpt.Models;
 
@@ -7,6 +8,8 @@ public partial class Cccd
 {
     public int CccdId { get; set; }
 
+    [StringLength(12, ErrorMessage = "Số CCCD phải có 12 chữ số")]
+    [Required(ErrorMessage = "Số CCCD không được để trống")]
     public string CccdNumber { get; set; } = null!;
 
     public string? ImageMt { get; set; }
